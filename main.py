71dafd7feb99c6d1d9f1bcd90463735ce4a1258f
Mazen Sayed <54520113+Mazensayed91@users.noreply.github.com>
2021-10-22 18:28:37 +0200

    added crud setup with login/signup

---
+++ main.py
@@ -0,0 +1,37 @@
+import time
+from pathlib import Path
+
+from fastapi import FastAPI, APIRouter, Request, Depends
+from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+
+import crud
+from api import deps
+from api.api_v1.api import api_router
+from core.config import settings
+
+BASE_PATH = Path(__file__).resolve().parent
+TEMPLATES = Jinja2Templates(directory=str(BASE_PATH / "templates"))
+
+root_router = APIRouter()
+app = FastAPI(title="Chatbot API", openapi_url=f"{settings.API_V1_STR}/openapi.json")
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
+
+app.include_router(api_router, prefix=settings.API_V1_STR)
+app.include_router(root_router)
+
+
+if __name__ == "__main__":
+    # Use this for debugging purposes only
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
